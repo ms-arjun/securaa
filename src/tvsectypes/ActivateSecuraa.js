e0f65bd729018ad64dcb9f6956e98b96015e2a9c
ms-arjun <arjunmurugesan11@gmail.com>
2024-01-29 17:26:42 +0530

    last

--- src/tvsectypes/ActivateSecuraa.js
+++ src/tvsectypes/ActivateSecuraa.js
@@ -1,8 +1,66 @@
 import React from 'react'
 
+
 const ActivateSecuraa = () => {
   return (
-    <div>ActivateSecuraa</div>
+    <>
+    <div className='flex justify-center items-center flex-col'>
+      <div className='text-8xl font-semibold'>
+        ACTIVATE SECURAA
+      </div>
+      <div>
+        <img src="https://tse4.mm.bing.net/th?id=OIP.jS1nqdviO5VYemLVUXHkggAAAA&pid=Api&P=0&h=180" alt=""  className='w-[800px] h-[300px]'/>
+      </div>
+      <div className='mt-10 '>
+        <p className='indent-[100px]'>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by
+           the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical
+            and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").Versions
+             of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by 
+             advertisements for Letraset transfer sheum was introduced to the digital world in the mid-1980s, when Aldus
+              employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other 
+              popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipss have many L
+              aTeX package web content managers such as Joomla! and WordPress, and CSS libraries such <br /> <br />  </p>
+
+             
+
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by
+           the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical
+            and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").Versions
+             of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by 
+             advertisements for Letraset transfer sheum was introduced to the digital world in the mid-1980s, when Aldus
+              employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other 
+              popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipss have many L
+              aTeX package web content managers such as Joomla! and WordPress, and CSS libraries such</p>
+
+
+           
+
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by
+           the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical
+            and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").Versions
+             of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by 
+             advertisements for Letraset transfer sheum was introduced to the digital world in the mid-1980s, when Aldus
+              employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other 
+              popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipss have many L
+              aTeX package web content managers such as Joomla! and WordPress, and CSS libraries such <br /> <br /></p>
+
+
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by
+           the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical
+            and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").Versions
+             of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by 
+             advertisements for Letraset transfer sheum was introduced to the digital world in the mid-1980s, when Aldus
+              employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other 
+              popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipss have many L
+              aTeX package web content managers such as Joomla! and WordPress, and CSS libraries such <br /> <br /></p>
+      </div>
+
+        <div className='bg-blue-400 px-8  py-4 rounded-full'>
+          <button>Activate</button>
+        </div>
+    </div>
+   
+    </>
   )
 }
 
